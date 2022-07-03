8a8a9116205da1664200e04c276f840c3f04d605
Francois <francoisdiel@hotmail.com>
2022-07-04 00:27:59 +0200

    navbar and onload animation

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,30 @@ window.addEventListener('scroll',function(){
 function toTop(){
     window.scrollTo({top:0,behavior:'smooth'})
 }
+
+const logo= document.querySelectorAll('.logo-main path');
+
+console.log(logo);
+for (let i = 0; i < logo.length; i++) {
+    console.log(logo[i].getTotalLength());
+}
+
+const nav = document.querySelector('nav');
+const frontSection = document.querySelector('.front-section');
+
+const frontSectionOptions = {
+    rootMargin: '-45px 0px 0px 0px'
+};
+
+const frontSectionObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            nav.classList.add('nav-past');
+        } else {
+            nav.classList.remove('nav-past');
+        }
+    });
+    }, frontSectionOptions);
+
+
+frontSectionObserver.observe(frontSection);
