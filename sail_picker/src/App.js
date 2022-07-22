5eff1f00ec9766295d5d20a7cab71f32ec4b1cda
Francois <francoisdiel@hotmail.com>
2022-07-22 22:17:10 +0200

    beginning of new subdomain
    I want to build a sail picker for surfing

---
+++ sail_picker/src/App.js
@@ -0,0 +1,11 @@
+import logo from './images/logo.svg';
+import './App.css';
+import Surfing from './components/surfing';
+
+function App() {
+  return (
+    <Surfing />
+  );
+}
+
+export default App;
